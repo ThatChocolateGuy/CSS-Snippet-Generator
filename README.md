1740da9af079019c0c9274ac64feb143161328f8
Nem <nemx14@gmail.com>
2019-02-11 04:02:31 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+#CSS Snippet Generator
+
+Generate formatted CSS which would otherwise be tedious to code by hand.
+
+Using variables and a bit of custom logic, you can generate repetitive code snippets with parameters that differ slightly from snippet-to-snippet.
+
+Clone the repo and replace the sample code in the <pre><pre></pre></pre> block with your own. Use the <pre><span class="nocode"></span></pre> blocks to play with your variables. You can inspect the existing logic tied to the <pre>id</pre> attributes (<pre>postNumber</pre> & <pre>thumbnail</pre>) for an example on how this works.
+
+Project utilizes the [!prettify.js](https://github.com/google/code-prettify) library by Google and [!Vim Sunburst Theme](https://github.com/google/code-prettify/blob/master/styles/sunburst.css) by David Leibovic.
---
+++ generator.js
@@ -0,0 +1,44 @@
+var thumbnail = 1; // Starting thumbnail image number
+var postNumber = 20; // Starting post ID
+const moduleCount = 3; // Number of snippets to display
+var storeVals = []; // Stores thumnail and post number values
+var storeCode = []; // Empty array to store CSS code blocks
+
+var css_block = css_block = document.getElementById("css-snippet"); // Stores CSS snippet template
+var thumbnail_output = document.getElementById("thumbnail");
+var postNum_output = document.getElementById("postNumber");
+var css_print = document.getElementById("css_print");
+
+thumbnail_output.innerHTML = thumbnail;
+postNum_output.innerHTML = postNumber;
+
+function generateCss() {
+		for (let module = 1; module < moduleCount; module++) {
+				thumbnail++;
+				postNumber++;
+
+				storeVals[module] = { thumbnail, postNumber };
+
+				console.log(module, storeVals[module]);
+		}
+}
+
+function printCss() {
+		document.getElementById("buttons").style.display = "none"; // Hide buttons
+
+		for (let module = 1; module < moduleCount; module++) {
+				var pre = document.createElement("pre");
+				var code = document.createElement("code");
+
+				code.innerHTML = css_block.innerHTML;
+				pre.className = "prettyprint container";
+				pre.appendChild(code);
+				pre.style.padding = "1em";
+				css_print.appendChild(pre); // Append new 'pre' block to css_print div
+
+				thumbnail_output.innerHTML = storeVals[module].thumbnail;
+				postNum_output.innerHTML = storeVals[module].postNumber;
+
+				console.log(module, storeVals[module]);
+		}
+}
